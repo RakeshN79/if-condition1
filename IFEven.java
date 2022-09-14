3f67a868addcd03918435aa2b1913f862203c261
RakeshN79 <113411352+RakeshN79@users.noreply.github.com>
2022-09-14 12:48:37 +0530

    Add files via upload

---
+++ IFEven.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class  IFEven
+{
+	public static void main(String[] args) 
+	{
+		Scanner scan=new Scanner(System.in);
+		int Number=scan.nextInt();
+		if ( Number/2==0)
+		{
+			System.out.println("even number it is:");
+		}
+		else
+		{
+		System.out.println("odd nuber it is:");
+		}
+	}
+}
